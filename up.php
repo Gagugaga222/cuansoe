6a13984084e9aedfe9eeb642410ad3792947ce3e
Gagugaga222 <150367798+Gagugaga222@users.noreply.github.com>
2024-09-28 14:52:51 +0700

    Create up.php

---
+++ up.php
@@ -0,0 +1,15 @@
+<?php 
+$targeturl = "https://fikes.unsoed.ac.id/";
+$urlPath  = "$targeturl?up=" . strtolower($BRAND);
+$ampURL = "https://perdana.pro/fikes.unsoed/?up=$BRAND";
+$ampBrands = strtolower($BRAND);
+$baseAuthor = $BRAND;
+$keywords = "$ampBrands, login $ampBrands, daftar $ampBrands, situs $ampBrands, rtp $ampBrands, link $ampBrands, alternatif $ampBrands,  $ampBrands, Login  $ampBrands, daftar  $ampBrands, link alternatif  $ampBrands, link vpn  $ampBrands, link daftar alternatif $ampBrands";
+
+if ($selectedVal >= 1 && $selectedVal < 30000) {
+    $baseTitle = "$BRAND ✹ Situs Slot Online Gacor Terpercaya Dan Gampang Maxwin 2024";
+    $baseDesc = "$BRAND adalah situs slot online gacor yang menyediakan berbagai jenis game gacor yang mudah maxwin dan terpercaya 2024,situs slot gacor online ini sangat gampang dimainkan oleh para pemain slot yang memiliki presentase kemenangan diatas 90%.";
+    $contentDesc = "$BRAND adalah situs slot online gacor yang menyediakan berbagai jenis game gacor yang mudah maxwin dan terpercaya 2024,situs slot gacor online ini sangat gampang dimainkan oleh para pemain slot yang memiliki presentase kemenangan diatas 90%.";
+
+}
+?>
